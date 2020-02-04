e85be4739087f487e27cbaa608ed76e77845d531
Francois Best <francois@francoisbest.com>
2020-02-04 09:46:04 +0100

    fix: Handle deployment error
    In cases where appID and JSON file are both present,
    an alias is explicitly needed for deployment.
    We re-use the appID as the alias.

--- src/main.ts
+++ src/main.ts
@@ -18,22 +18,27 @@ async function run(): Promise<void> {
     if (!secret) {
       throwMissingEnvVar('CLEVER_SECRET')
     }
+
+    // There is an issue when there is a .clever.json file present
+    // and only the appID is passed: link will work, but deploy will need
+    // an alias to know which app to publish. In this case, we set the alias
+    // to the appID (if it was not already specified).
     const appID = core.getInput('appID')
-    const alias = core.getInput('alias')
-    // Will be copied from deps at build time
+    const alias = core.getInput('alias') || appID
     const cleverCLI = path.resolve(__dirname, '../node_modules/.bin/clever')
+    core.debug(`Clever CLI path: ${cleverCLI}`)
+
+    // Authenticate (this will only store the credentials at a known location)
     await exec(cleverCLI, ['login', '--token', token, '--secret', secret])
 
     if (appID) {
-      const args = alias ? ['link', appID, '--alias', alias] : ['link', appID]
       core.debug(`Linking ${appID}`)
-      await exec(cleverCLI, args)
+      await exec(cleverCLI, ['link', appID, '--alias', alias])
     }
 
     const args = ['deploy']
     if (alias) {
-      args.push('--alias')
-      args.push(alias)
+      args.push('--alias', alias)
     }
     await exec(cleverCLI, args)
   } catch (error) {
