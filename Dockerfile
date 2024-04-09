27043ae5968d9e543de78750f9dc514934331fd6
Francois Best <github@francoisbest.com>
2024-04-09 13:20:54 +0200

    chore: Dubious ownership needs to be fixed at runtime

--- Dockerfile
+++ Dockerfile
@@ -18,13 +18,6 @@ FROM node:20-slim AS final
 
 RUN apt update && apt install -y git
 
-# Fix for GitHub Actions
-# fatal: detected dubious ownership in repository at '/github/workspace'
-# To add an exception for this directory, call:
-RUN mkdir -p /github/workspace && git config --global --add safe.directory /github/workspace
-
-WORKDIR /github/workspace
-
 COPY --from=builder /action/package.json /action/package.json
 COPY --from=builder /action/node_modules /action/node_modules
 COPY --from=builder /action/dist /action/dist
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import * as core from '@actions/core'
+import { exec } from '@actions/exec'
 import run, { processArguments } from './action'
 
 async function main(): Promise<void> {
   try {
+    await fixGitDubiousOwnership()
     const args = processArguments()
     return await run(args)
   } catch (error) {
@@ -12,6 +14,11 @@ async function main(): Promise<void> {
       core.setFailed(String(error))
     }
   }
+}
+
+// https://www.kenmuse.com/blog/avoiding-dubious-ownership-in-dev-containers/
+function fixGitDubiousOwnership() {
+  return exec('git', ['config', '--global', '--add', 'safe.directory', '/github/workspace'])
 }
 
 main()
